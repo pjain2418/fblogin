baef353006026983b0a42cef9ec215716cf74a16
pjain2418 <pjain2418@gmail.com>
2022-03-09 23:47:44 +0530

    Initial commit

---
+++ app/src/main/java/com/pratik/fblogin/RegisterActivity.java
@@ -0,0 +1,29 @@
+package com.pratik.fblogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        Button signup = findViewById(R.id.signup);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
+                startActivity(intent);
+                Toast.makeText(RegisterActivity.this, "Sign Up Successful", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
